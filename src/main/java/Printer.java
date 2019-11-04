dabd001e08227aeccc67a8c42acff70bdf56f019
cjwales <cjwales@gmail.com>
2019-11-04 20:38:56 +0000

    finished printer

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,11 +1,25 @@
 public class Printer {
-    private int paper;
 
-    public Printer(int paper) {
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner) {
         this.paper = paper;
+        this.toner = toner;
     }
 
-    public void print(int paper, int copies) {
+    public void print(int pages, int copies) {
+        if (paper >= pages * copies) {
+            this.toner -= pages * copies;
+            this.paper -= pages * copies;
+        }
+    }
 
+    public int getPaper() {
+        return this.paper;
+    }
+
+    public int getToner() {
+        return this.toner;
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,3 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class PrinterTest {
     Printer myPrinter;
+
+    @Before
+    public void before() {
+        myPrinter = new Printer(100, 100);
+    }
+
+    @Test
+    public void getPaper() {
+        assertEquals(100, myPrinter.getPaper());
+    }
+
+    @Test
+    public void canPrint() {
+        myPrinter.print(2, 2);
+        assertEquals(96, myPrinter.getPaper());
+        assertEquals(96, myPrinter.getToner());
+    }
 }
