59e193b7775e74b3872a43d09d89e51383359f56
cjwales <cjwales@gmail.com>
2019-11-04 17:53:12 +0000

    added printer stuff

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,3 +1,11 @@
 public class Printer {
+    private int paper;
 
+    public Printer(int paper) {
+        this.paper = paper;
+    }
+
+    public void print(int paper, int copies) {
+
+    }
 }
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,3 @@
+public class PrinterTest {
+    Printer myPrinter;
+}
